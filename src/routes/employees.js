443ff6eea13f6f930b8af88267b1a1581b779e51
andres2018151725 <andres05022000@gmail.com>
2019-11-21 15:15:57 -0800

    D

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -41,6 +41,10 @@ router.get('/employees/registerSite', (req, res) => {
     res.render('employees/registerSite');
 });
 
+router.get('/employees/registerProducts', (req, res) => {
+    res.render('employees/registerProducts');
+});
+
 
 
 module.exports = router;
