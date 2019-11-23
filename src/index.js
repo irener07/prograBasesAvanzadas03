0652cf78861cfeac8f78de2e405a75c1316ba7bb
irener07 <rojasirene070@gmail.com>
2019-11-22 22:53:28 -0600

    autocomplete:3

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ app.use(require('./routes/index'));
 app.use(require('./routes/employees'));
 app.use(require('./routes/clients'));
 app.use(require('./routes/supermarkets'));
+app.use(require('./routes/sites'));
 app.use(express.static(__dirname + '/public'));
 app.use(router);
 app.listen(config.PORT, ()=> console.log(`Server on port ${config.PORT}`));
