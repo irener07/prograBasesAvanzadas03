e541089c27ff90e8fe26a7f24377131ac7bf5c60
andres2018151725 <andres05022000@gmail.com>
2019-11-24 10:32:04 -0800

    jjj

--- src/configuration/migration.js
+++ src/configuration/migration.js
@@ -5,13 +5,58 @@ const session = driver.session();
 
 const clients = require('../models/clients');
 const supermarkets = require('../models/supermarkets');
+const orders = require('../models/orders');
 
 module.exports = async()=>{
 
+
+    // Insert markets
     session
         .run('MATCH (n)DETACH DELETE n')
         .then(function(result){
             session.close();                
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+
+    const mongoSupermarkets = await supermarkets.find();
+    for(var i = 0; i < mongoSupermarkets.length; i++){
+        const supermarket = mongoSupermarkets[i];
+        const idSuperMarket = supermarket.idSuperMarket;
+        const name = supermarket.name;
+        const description = supermarket.description;
+        const address = supermarket.address;
+        const latitude = supermarket.latitude.toString();
+        const longitude = supermarket.longitude.toString();
+        const typeSuperMarket = supermarket.typeSupermarket;
+        const telephone = supermarket.telephone;
+        const rating = supermarket.rating;
+        const schedule = supermarket.schedule;
+        const website = supermarket.website;
+
+         session
+        .run('CREATE (n:supermarkets{idSuperMarket:{idSuperMarketParam},name:{nameParam},description:{descriptionParam},address:{addressParam},latitude:{latitudeParam},longitude:{longitudeParam},typeSuperMarket:{typeSuperMarketParam},telephone:{telephoneParam},rating:{ratingParam},schedule:{scheduleParam},website:{websiteParam}}) Return n',
+         {idSuperMarketParam:idSuperMarket,nameParam:name,descriptionParam:description,addressParam:address,latitudeParam:latitude,longitudeParam:longitude,typeSuperMarketParam:typeSuperMarket,telephoneParam:telephone,ratingParam:rating,scheduleParam:schedule,websiteParam:website})
+        
+         .then(result =>{
+            session.close();   
+          
+            // on application exit:
+            driver.close();             
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    };
+
+    //Insert clients
+    session
+        .run('MATCH (n)DETACH DELETE n')
+        .then(function(result){
+            session.close();   
+
+                       
         })
         .catch(function(err){
             console.log(err);
@@ -45,51 +90,8 @@ module.exports = async()=>{
           });
     };
 
-    session
-        .run('MATCH (n)DETACH DELETE n')
-        .then(function(result){
-            session.close();                
-        })
-        .catch(function(err){
-            console.log(err);
-        })
 
-    const mongoSupermarkets = await supermarkets.find();
-    for(var i = 0; i < mongoSupermarkets.length; i++){
-        const supermarket = mongoSupermarkets[i];
-        const idSuperMarket = supermarket.idSuperMarket;
-        const name = supermarket.name;
-        const description = supermarket.description;
-        const address = supermarket.address;
-        const latitude = supermarket.latitude.toString();
-        const longitude = supermarket.longitude.toString();
-        const typeSuperMarket = supermarket.typeSupermarket;
-        const telephone = supermarket.telephone;
-        const rating = supermarket.rating;
-        const schedule = supermarket.schedule;
-        const website = supermarket.website;
-
-        const resultPromise = session
-        .run('CREATE (n:supermarkets{idSuperMarket:{idSuperMarketParam},name:{nameParam},description:{descriptionParam},address:{addressParam},latitude:{latitudeParam},longitude:{longitudeParam},typeSuperMarket:{typeSuperMarketParam},telephone:{telephoneParam},rating:{ratingParam},schedule:{scheduleParam},website:{websiteParam}) Return n',
-         {idSuperMarketParam:idSuperMarket,nameParam:name,descriptionParam:description,addressParam:address,latitudeParam:latitude,longitudeParam:longitude,typeSuperMarketParam:typeSuperMarket,telephoneParam:telephone,ratingParam:rating,scheduleParam:schedule,websiteParam:website})
-        
-         resultPromise.then(result =>{
-            session.close();   
-            const singleRecord = result.records[0];
-            const node = singleRecord.get(0);
-          
-            console.log(node.properties.name);
-          
-            // on application exit:
-            driver.close();             
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-    };
-
-        
-/*
+//Insert orders
     session
         .run('MATCH (n)DETACH DELETE n')
         .then(function(result){
@@ -101,26 +103,62 @@ module.exports = async()=>{
 
     const mongoOrders = await orders.find();
 
-    
-
-
     for(var i = 0; i < mongoOrders.length; i++){
         const order = mongoOrders[i];
 
-        
-        const products = order.products.toString();
-        const dateTime = order.dateTime.toString();
+     
+        const idOrden = order.id;
         //console.log(date);
+        const date = order.date.toString();
+        const time = order.time.toString();
         const status = order.status;
         const particularNeeds = order.particularNeeds;
         const idClient = order.idClient;
         const idSuperMarket = order.idSuperMarket;
-        const totalAmount = order.totalAmount;
+        const totalAmount = order.totalAmount.toString();
 
         const resultPromise = session
-        .run('CREATE (n:clients {id:{idParam},firstName:{firstNameParam},lastName:{lastNameParam},birthDate:{birthDateParam},email:{emailParam},password:{passwordParam},telephone:{telephoneParam}}) Return n',
-         {idParam:id,firstNameParam:firstName,lastNameParam:lastName,birthDateParam:birthDate,emailParam:email,passwordParam:password,telephoneParam:telephone})
-        
+        .run('CREATE (n:orders {idOrden:{idParam},date:{dateParam},status:{statusParam},particularNeeds:{particularNeedsParam},idClient:{idClientParam},idSuperMarket:{idSuperMarketParam},totalAmount:{totalAmountParam}}) Return n',
+         {idParam:idOrden, dateParam:date, timeParam:time, statusParam:status, particularNeedsParam:particularNeeds,idClientParam:idClient,idSuperMarketParam:idSuperMarket, totalAmountParam:totalAmount })
+
+
+         const mongoProductsOrder = order.products();
+ 
+         for(var i = 0; i < mongoProductsOrder.length; i++){
+             const product = mongoProductsOrder[i];
+             const idProduct = product.productID;
+             const nameProduct = product.productName;
+             const amountProduct = product.amount;
+
+             const resultPromise = session
+             .run('CREATE (n:products {idProduct:{idParam},name:{nameParam},amount:{amountParam}}) Return n',
+             {idParam:idProduct, nameParam:nameProduct, amountParam:amountProduct })
+                session.close();
+              
+                const singleRecord = result.records[0];
+                const node = singleRecord.get(0);
+              
+                console.log(node.properties.name);
+              
+                // on application exit:
+                driver.close();
+              
+             
+             const resultPromiseb = session
+             .run('MATCH (a:orders {idOrden:{idOrdenParam}}),(b:products{idProduct:{idProductParam}}) MERGE(b)-[r:LEAVES_FROM]-(a) RETURN a,b', {idProduct:idProduct, idOrder:idProduct})
+             resultPromiseb.then(result => {
+                session.close();
+              
+                const singleRecord = result.records[0];
+                const node = singleRecord.get(0);
+              
+                console.log(node.properties.name);
+              
+                // on application exit:
+                driver.close();
+              });
+              
+         }
          resultPromise.then(result => {
             session.close();
           
@@ -135,81 +173,48 @@ module.exports = async()=>{
     };
 
 
+    //Connection between orders and markets
+
     for(var i = 0; i < mongoOrders.length; i++){
         const order = mongoOrders[i];
-        //console.log(delivery);
+        const idOrderCmp= order.idSuperMarket;     
+        for(var j = 0; j < mongoSupermarkets.length; j++){
+            const supermarket = mongoSupermarkets[j];
+            const idSuperMarketCmp = supermarket. idSuperMarket;
 
-        const id = order.id.toString();
-        //console.log(idDelivery);
-        const products = order.products.toString();
-        const dateTime = order.dateTime.toString();
-        //console.log(date);
-        const status = order.status;
-        const particularNeeds = order.particularNeeds;
-        const idClient = order.idClient;
-        const idSuperMarket = order.idSuperMarket;
-        const totalAmount = order.totalAmount;
+            if (idSuperMarketCmp== idOrderCmp){
+                session
+                    .run('MATCH (a:orders {id:{idParam}}),(b:supermarkets {idSuperMarket:{idSuperMarketParam}}) MERGE(a)-[r:LEAVES_FROM]-(b) RETURN a,b', {idSuperMarketParam:idSuperMarketCmp, idParam:idOrderCmp})
+                    .then(function(result){
+                        session.close();                
+                    })
+                    .catch(function(err){
+                        console.log(err);
+                    })
+        
+            };
 
-        session
-        .run('CREATE (n:orders {id:{idParam},products:{productsParam},dateTime:{dateTimeParam},status:{statusParam},particularNeeds:{needsParam},idClient:{idClientParam},idSuperMarket:{idSuperMarketParam},totalAmount:{totalAmountParam}}) Return n',
-         {idParam:id,productsParam:products,dateTimeParam:dateTime,statusParam:status,needsParam:particularNeeds,idClientParam:idClient,idSuperMarketParam:idSuperMarket,totalAmountParam:totalAmount})
-        .then(function(result){
-            session.close();                
-        })
-        .catch(function(err){
-            console.log(err);
-        })
+      };
+
     };
 
-    const mongoSupermarkets = await supermarkets.find();
+    
 
-    for(var i = 0; i < mongoSupermarkets.length; i++){
-        const supermarket = mongoSupermarkets[i];
-        //console.log(place);
-        const idSuperMarket = supermarket.idSuperMarket;
-        const name = supermarket.name;
-        const description = supermarket.description;
-        const address = supermarket.address;
-        const latitude = supermarket.latitude;
-        const longitude = supermarket.longitude;
-        const typeSuperMarket = supermarket.typeSuperMarket;
-        const image = supermarket.image;
-        const telephone = supermarket.telephone;
-        const rating = supermarket.rating;
-        const schedule = supermarket.schedule;
-        const website = supermarket.website;
-        const products = supermarket.products;
-
-        session
-        .run('CREATE (n:supermarkets{idSuperMarket:{idSuperMarketParam},name:{nameParam},description:{descriptionParam},address:{addressParam},latitude:{latitudeParam},longitude:{longitudeParam},typeSuperMarket:{typeSuperMarketParam},image:{imageParam},telephone:{telephoneParam},rating:{ratingParam},schedule:{scheduleParam},website:{websiteParam},products:{productsParam}}) Return n',
-         {idSuperMarketParam:idSuperMarket,nameParam:name,descriptionParam:description,addressParam:address,latitudeParam:latitude,longitudeParam:longitude,typeSuperMarketParam:typeSuperMarket,imageParam:image,telephoneParam:telephone,ratingParam:rating,scheduleParam:schedule,websiteParam:website,productsParam:products})
-        .then(function(result){
-            session.close();                
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-    };
-
-    //Relacion entre las ordenes y los clientes
+    //Connection betwwen orders and clients
 
     for(var i = 0; i < mongoClients.length; i++){
-        //console.log(adminMongoClients);
         const client = mongoClients[i];
-        //console.log(client);
-        const id = client.id;
-        //console.log("Pto1: email");
-        //console.log(email);
-
+        const idClient = client.id;
+        
         for(var j = 0; j < mongoOrders.length; j++){
             const order = mongoOrders[j];
-            const idOrder = order.id.toString();
-            const idClientOrder = order.idClient;
+            const idOrder = order.idClient;
+    
 
-            if(idClientOrder==id){
-
+            if(idOrder==idClient){
+                const comparator = idOrder;
                 session
-                    .run('MATCH (a:clients {id:{idParam}}),(b:orders {id:{idParam}}) MERGE(a)-[r:ORDER]-(b) RETURN a,b', {idParam:id, idParam:idOrder})
+                    .run('MATCH (a:clients {id:{idParamC}}),(b:orders {id:{idParamO}}) MERGE(a)-[r:ORDER]-(b) RETURN a,b', {idParamC:idClient, idParamO:idOrder})
                     .then(function(result){
                         //console.log(idUser);
                         //console.log(idDelivery);
@@ -218,42 +223,32 @@ module.exports = async()=>{
                     .catch(function(err){
                         console.log(err);
                     })
-                //console.log("Pto3: R made");
+
+
+                for(var k = 0; k < mongoOrders.length; k++){
+
+                    const idSuperMarketP = order.idSuperMarket[k];
+        
+
+                    if(idSuperMarketP==comparator){
+
+                        session
+                            .run('MATCH (a:supermarkets {id:{idParamM}}),(b:clients {id:{idParamC}}) MERGE(a)-[r:ORDER]-(b) RETURN a,b', {idParamM:comparator, idParamC:comparator})
+                            .then(function(result){
+                                //console.log(idUser);
+                                //console.log(idDelivery);
+                                session.close();                
+                            })
+                            .catch(function(err){
+                                console.log(err);
+                            })
+
+                    };
+
+                };
             };
         };
     };
 
-    //Relacion entre ordenes y supermercados
-
-    for(var i = 0; i < mongoOrders.length; i++){
-        const order = mongoOrders[i];
-        //console.log(delivery);
-
-        const id = order.id.toString();
-        //console.log("Pto1: idDelivery");
-        //console.log(idDelivery);
-        const idSuperMarketOrder = order.idSuperMarket;
-        //console.log("Pto1: idPlaceDelivery");
-        //console.log(idPlaceDelivery);
-
-        for(var j = 0; j < mongoSupermarkets.length; j++){
-            const supermarket = mongoSupermarkets[j];
-            const idSuperMarket = supermarket.idSuperMarket;
-
-            if (idSuperMarketOrder == idSuperMarket){
-                session
-                    .run('MATCH (a:orders {id:{idParam}}),(b:supermarkets {idSuperMarket:{idSuperMarketParam}}) MERGE(a)-[r:LEAVES_FROM]-(b) RETURN a,b', {idSuperMarketParam:idSuperMarket, idParam:id})
-                    .then(function(result){
-                        session.close();                
-                    })
-                    .catch(function(err){
-                        console.log(err);
-                    })
-                //console.log("Pto3: R made");
-            };
-
-      };
-
-    };*/
 
 }
