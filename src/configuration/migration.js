29341ddfdc0515b3ba9dce25a8a96c6330affa09
GabrielCMCR <gabrielred.cm@gmail.com>
2019-11-25 00:54:07 -0600

    jjjj

--- src/configuration/migration.js
+++ src/configuration/migration.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '6564'));
+const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Abcd1234'));
 const session = driver.session();
 
 const clients = require('../models/clients');
